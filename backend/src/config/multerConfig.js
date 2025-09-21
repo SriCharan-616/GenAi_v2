5ff78edc748a9b41764268ef1ba2a96c2d3a5956
noname <noname@gmail.com>
2025-09-21 11:35:16 +0530

    upload to insta added

--- backend/src/config/multerConfig.js
+++ backend/src/config/multerConfig.js
@@ -1,6 +1,16 @@
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "./cloudinaryConfig.js";
 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "your-app-images",
+    allowed_formats: ["jpg","jpeg","png","webp"]
+  },
+});
 
-const upload = multer({ storage: multer.memoryStorage() });
+const upload = multer({ storage });
 
-export default upload;
+// Middleware for uploading image only
+export const uploadImageMiddleware = upload.single("image");
--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { uploadProduct } from "../controllers/productController.js";
-import upload from "../config/multerConfig.js";
+import upload from "../config/multerConfig1.js";
+import { uploadImageMiddleware } from "../config/multerConfig.js";
 
 const router = express.Router();
 
+router.post("/upload", uploadImageMiddleware, (req, res) => {
+  res.json({
+    success: true,
+    message: "Image uploaded to Cloudinary!",
+    fileUrl: req.file?.path || req.file?.secure_url
+  });
+});
+
 // Upload multiple images
-router.post("/upload", upload.array("images", 10), uploadProduct); // max 10 images
+router.post("/uploadprod", upload.array("images", 10), uploadProduct); // max 10 images
 
 export default router;

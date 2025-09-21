9ed20022d980b0a8bdd1539ae4e6e481389e09c8
Sricharan <sricharanjayanthizz2gmail.com>
2025-09-21 20:13:29 +0530

    Frontend Ready for Deployment

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -239,7 +239,6 @@ const Login = () => {
                     className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
                     tabIndex={-1}
                   >
-                    {showPassword ? '🙈' : '👁️'}
                   </button>
                 </div>
                 {errors.password && (
